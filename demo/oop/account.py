64195031b19404ecd40094b7414222ba9cc6bd08
srikanthpragada <srikanthpragada@gmail.com>
2021-01-07 07:55:15 +0530

    07-JAN

---
+++ demo/oop/account.py
@@ -0,0 +1,26 @@
+class SavingsAccount:
+    minbal = 10000
+    intrate = 2.5
+
+    def __init__(self, acno, ahname):
+        self.acno = acno
+        self.ahname = ahname
+        self.balance = 0
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if self.balance - SavingsAccount.minbal < amount:
+            raise ValueError("Insufficient Funds")
+
+        self.balance -= amount
+
+    def getbalance(self):
+        return self.balance
+
+
+s = SavingsAccount(1, "James")
+s.deposit(10000)
+s.withdraw(15000)
+print(s.getbalance())
--- demo/oop/student.py
+++ demo/oop/student.py
@@ -1,9 +1,25 @@
 class Student:
+    # Static attributes or class attributes
+    courses = {'Python': 10000, 'Java': 15000, 'DS': 20000}
+    gst = 12.5
+
+    @staticmethod
+    def gettotalfee(course):
+        fee = Student.courses.get(course,0)
+        return fee + fee * Student.gst / 100
+
+    @staticmethod
+    def getcoursecount():
+        return len(Student.courses)
+
     # Constructor
     def __init__(self, admno, name, course="Python"):
         # Object attributes
         self.admno = admno
         self.name = name
+        if course not in Student.courses:
+            raise ValueError("Invalid course name!")
+
         self.course = course
         self.feepaid = 0
 
@@ -12,10 +28,7 @@ class Student:
         return f"{self.admno}, {self.name}, {self.course}"
 
     def totalfee(self):
-        if self.course == 'Python':
-            return 10000
-        else:
-            return 15000
+        return Student.gettotalfee(self.course)
 
     def payment(self, amount):
         self.feepaid += amount
@@ -24,13 +37,13 @@ class Student:
         return self.totalfee() - self.feepaid
 
 
+print(Student.gettotalfee(''))  # Call static method
+print(Student.getcoursecount())
 s1 = Student(1, "Scott")  # Create an object
 s1.payment(5000)
 print(s1.due())
 
 print(s1.info())  # call method
 s2 = Student(2, "Tom", "Java")
-s2.course = "Python"
 print(s2.info())
-
-print(type(s1))
+print(s2.due())
