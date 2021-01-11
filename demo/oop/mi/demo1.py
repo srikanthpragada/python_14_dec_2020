99ed0407376eef4364d272fd510a6bf3e3f569c2
srikanthpragada <srikanthpragada@gmail.com>
2021-01-11 07:53:26 +0530

    11-JAN

---
+++ demo/oop/mi/demo1.py
@@ -0,0 +1,16 @@
+class A:
+    def process(self):
+        print("Process in A")
+
+
+class B:
+    def task(self):
+        print("Task in B")
+
+
+class C(A, B):
+    pass
+
+obj = C()
+obj.process()
+obj.task()
